5bde494197da1b9f6c59e44cba7e6d65c5e75d2f
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-09-05 21:05:48 -0300

    (feat) first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-form-testing';
+export class AppComponent implements OnInit {
+  public formParent: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.initFormParent();
+  }
+
+  private initFormParent(): void {
+    this.formParent = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      skills: new FormArray([], [Validators.required])
+    })
+  }
+
+  initFormSkill(): FormGroup {
+    return new FormGroup(
+      {
+        language: new FormControl(''),
+        projectURL: new FormControl(''),
+        expYear: new FormControl('', [Validators.required])
+      }
+    )
+  }
+
+  addSkill(): void {
+    const refSkills = this.formParent.get('skills') as FormArray;
+    refSkills.push(this.initFormSkill());
+  }
+
+
+  // NOTE: Obtener referencia a un formControl
+  getCtrl(key: string, form: FormGroup): any {
+    return form.get(key);
+  }
+
+  // NOTE: Quitar validaciones
+  removeValidation(index: number, key: string): void {
+    const refParent = this.formParent.get('skills') as FormArray;
+    const refSingle = refParent.at(index).get(key) as FormGroup;
+    refSingle.clearValidators();
+    refSingle.updateValueAndValidity();
+  }
+
+  // NOTE: Agregar validaciones
+  addValidation(index: number, key: string): void {
+    const refParent = this.formParent.get('skills') as FormArray;
+    const refSingle = refParent.at(index).get(key) as FormGroup;
+
+    refSingle.setValidators([
+      Validators.required
+    ])
+    refSingle.updateValueAndValidity();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TrimValueDirective } from './directives/trim-value.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrimValueDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
